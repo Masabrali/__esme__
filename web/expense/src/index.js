d5465580072b6fd58815fe0a1ee1992dcf246849
Masasi Mgengeli <masabrali@gmail.com>
2024-11-27 15:05:41 +0300

    Fixed core errors and warnings. Still checking on some routing and logical errors

--- web/expense/src/index.js
+++ web/expense/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import { createStore, applyMiddleware, compose ,combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import * as thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 import userReducer from './store/reducers/user';
 import voucherReducer from './store/reducers/voucher';
 import adminReducer from './store/reducers/admin';
@@ -29,11 +29,11 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <Provider store={store}><App /></Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
